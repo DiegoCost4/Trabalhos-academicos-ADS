c80f1d1302f3d656c5be5f1621cdda08652e1223
Diego <diegocostaferreiradasilva@gmail.com>
2022-10-04 22:23:22 -0300

    Add screen collaborator-registration - Diego C.

---
+++ ADS/collaborator-registration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void CadastroColaborador(){
+	
+	
+	char Name[10];
+	char lastName[15];
+	
+	printf("Digite seu nome: ");
+	scanf("%c",&Name);
+	
+	printf("Digite seu sobrenome: ");
+	scanf("%c",&lastName);
+	
+	
+	
+}
