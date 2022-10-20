f470f328df3871aca64dde2f15dc327f8b94930c
Diego <diegocostaferreiradasilva@gmail.com>
2022-10-20 19:17:33 -0300

    Atualizações - Diego

---
+++ ADS/definition.c
@@ -0,0 +1,12 @@
+void definition(){
+	
+	typedef struct {
+		
+	char nameClient[50];
+	char lastName[50];
+	char NumberClient[11];
+	char email[30];
+	float cpf[11];
+	}Pessoa;
+
+}
--- ADS/main-file.c
+++ ADS/main-file.c
@@ -2,10 +2,25 @@
 #include <locale.h>
 #include "home-menu.c"
 #include "option-menu.c"
-/// @brief 
+//#include "definition.c"
+
+/// @brief 0
 /// @return 
+
+typedef struct {
+		
+	char nameClient[50];
+	char lastName[50];
+	char NumberClient[11];
+	char email[30];
+	float cpf[11];
+	}Pessoa;
+
+
+
+
 int main() {
-	
+	//definition();
 	setlocale(LC_ALL, "Portuguese");
 	MenuPrincipal(); // Cadastro/Login
 	MenuOpcoes(); // Opções Para Nevegação
